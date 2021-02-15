92d4420e96fafa33bdc23726a0da4d2af0082dfc
Cartheme <mrmarius77@gmail.com>
2021-02-15 16:13:50 +0100

    test_loggetInn() og testLoggetInnFeil() er ferdig

--- src/test/java/oslomet/testing/EnhetstestSikkerhetsController.java
+++ src/test/java/oslomet/testing/EnhetstestSikkerhetsController.java
@@ -3,12 +3,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.mock.web.MockHttpSession;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -40,4 +46,50 @@ public class EnhetstestSikkerhetsController {
         // assert
         assertEquals("OK", resultat);
     }
+
+    @Test
+    public void test_LoggetInn(){
+        Map<String,Object> attributes = new HashMap<String,Object>();
+
+        doAnswer(new Answer<Object>(){
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                return attributes.get(key);
+            }
+        }).when(session).getAttribute(anyString());
+
+        doAnswer(new Answer<Object>(){
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                String key = (String) invocation.getArguments()[0];
+                Object value = invocation.getArguments()[1];
+                attributes.put(key, value);
+                return null;
+            }
+        }).when(session).setAttribute(anyString(), any());
+
+        // arrange
+        session.setAttribute("Innlogget", "12345678901");
+
+
+        // act
+        String resultat = sikkerhetsController.loggetInn();
+
+        // assert
+        assertEquals("12345678901", resultat);
+    }
+
+    @Test
+    public void test_LoggetInnFeil() {
+        // arrange
+        session.setAttribute("Ikke innlogget", null);
+
+        // act
+        String resultat = sikkerhetsController.loggetInn();
+
+        // assert
+        assertNull(resultat);
+    }
+
 }
