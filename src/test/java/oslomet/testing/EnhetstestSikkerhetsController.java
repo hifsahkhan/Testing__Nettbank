6624594ae1ab2cf110fe02901cc782f69651ab38
zuhuurmoe <56063635+zuhuurmoe@users.noreply.github.com>
2021-02-15 16:25:21 +0100

    sikkerhet

--- src/test/java/oslomet/testing/EnhetstestSikkerhetsController.java
+++ src/test/java/oslomet/testing/EnhetstestSikkerhetsController.java
@@ -41,6 +41,51 @@ public class EnhetstestSikkerhetsController {
         assertEquals("OK", resultat);
     }
     @Test
+    public void loggInn_FeilPersonnummer(){
+        session.setAttribute("Ikke innlogget","12345678901");
+
+        // arrange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i personnummer");
+
+        //act
+        String resultat = sikkerhetsController.sjekkLoggInn("1234567890","HeiHeiHei");
+
+        //assert
+        assertEquals("Feil i personnummer",resultat);
+    }
+
+    @Test
+    public void loggInn_FeilPassord(){
+        session.setAttribute("Ikke innlogget", "12345678901");
+        session.setAttribute("Ikke innlogget", "HeiHeiHei");
+
+
+        // arrange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i passord");
+
+        //act
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","HeiHei");
+
+        //assert
+        assertEquals("Feil i passord",resultat);
+
+    }
+
+    @Test
+    public void loggInn_FeilPersonnummerEllerPassord(){
+        //    session.setAttribute("");
+        // arrange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i personnummer eller passord");
+
+        //act
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","HeiHeiHei");
+
+        //assert
+        assertEquals("Feil i personnummer eller passord",resultat);
+
+    }
+
+    @Test
     public void test_sjekkLoggInnAdmin(){
 
 
@@ -72,4 +117,5 @@ public class EnhetstestSikkerhetsController {
         String resluts = sikkerhetsController.sjekkLoggInn("1235678901", "HeiHeiHei");
         assertEquals("OK", resluts);
     }
+
 }
