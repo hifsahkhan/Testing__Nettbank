8fee917fcdc241548c60b1acd56b46f65ed8a35e
Hifsah Khan <s344235@oslomet.no>
2021-02-15 16:06:00 +0100

    feil

--- src/test/java/oslomet/testing/EnhetstestSikkerhetsController.java
+++ src/test/java/oslomet/testing/EnhetstestSikkerhetsController.java
@@ -3,11 +3,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.mock.web.MockHttpSession;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -40,4 +46,54 @@ public class EnhetstestSikkerhetsController {
         // assert
         assertEquals("OK", resultat);
     }
+
+    @Test
+    public void loggInn_FeilPersonnummer(){
+        session.setAttribute("Ikke innlogget","12345678901");
+
+        // arrange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i personnummer");
+
+        //act
+        String resultat = sikkerhetsController.sjekkLoggInn("1234567890","HeiHeiHei");
+
+        //assert
+        assertEquals("Feil i personnummer",resultat);
+    }
+
+    @Test
+    public void loggInn_FeilPassord(){
+        session.setAttribute("Ikke innlogget", "12345678901");
+        session.setAttribute("Ikke innlogget", "HeiHeiHei");
+
+
+        // arrange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i passord");
+
+        //act
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","HeiHei");
+
+        //assert
+        assertEquals("Feil i passord",resultat);
+
+    }
+
+    @Test
+    public void loggInn_FeilPersonnummerEllerPassord(){
+    //    session.setAttribute("");
+        // arrange
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i personnummer eller passord");
+
+        //act
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","HeiHeiHei");
+
+        //assert
+        assertEquals("Feil i personnummer eller passord",resultat);
+
+    }
+
+
+
+
+
 }
